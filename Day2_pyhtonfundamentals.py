772bc8a0879d473ad929f8a799b98abf6d98daf1
Salif_Shaikh <73948800+Salif-Shaikh@users.noreply.github.com>
2021-06-26 14:51:27 +0530

    Add files via upload

---
+++ Day2_pyhtonfundamentals.py
@@ -0,0 +1,9 @@
+#Rule 1 no namespace in variable
+#Rule 2 variable not starting with number
+#Rule 3 variable not containing special character
+# String immutable datatype def with single,double,triple qoutes
+Displaystring='having no namespace in it, Using single qoute ' 
+string1="\nhaving number after the variable, Using double qoute"
+string="""\nUsing triple qoutes"""
+
+print(Displaystring,string1,string)
