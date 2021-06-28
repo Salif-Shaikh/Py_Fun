f17277819662eb04fd7116fc3262c26449aa3179
Salif_Shaikh <73948800+Salif-Shaikh@users.noreply.github.com>
2021-06-28 13:53:37 +0530

    Add files via upload

---
+++ Day3_pythonfundametals.py
@@ -0,0 +1,11 @@
+#Using hashtag for comment.
+string1="hello"
+print(type(string1))
+print(string1.title())
+print(string1.upper())
+string2="PYTHON"
+print(string2.lower())
+string = f"{string1} for {string2}"
+print(string)
+#using \n
+print("Languages:\nC\nC++\nPython\njava")
